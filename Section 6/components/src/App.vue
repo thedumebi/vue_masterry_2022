fc826889b493bcad4807211c2a4225e7387bd15a
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-25 18:35:39 +0100

    components

---
+++ Section 6/components/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>Hey!</h3>
+
+  <Greeting :age="age" />
+  <user :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB"/>
+</template>
+
+<script>
+import Greeting from "./components/Greeting.vue";
+import User from "./components/User.vue";
+
+export default {
+  name: "App",
+  components: {
+    Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 23,
+    };
+  },
+  methods: {
+    updateAge(num) {
+      this.age += num;
+    },
+    updateAgeCB(num) {
+      this.age += num;
+    },
+  },
+};
+</script>
