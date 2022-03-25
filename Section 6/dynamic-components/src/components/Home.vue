a728d3e395f866c0d8e2d37623624b7710970845
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-25 22:46:26 +0100

    section 6-complete

---
+++ Section 6/dynamic-components/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home component unmounted");
+  },
+  activated() {
+      console.log("Home conponent activated");
+  },
+  deactivated() {
+      console.log("Home conponent deactivated");
+  }
+};
+</script>
