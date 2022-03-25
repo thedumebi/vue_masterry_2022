77380ab27f12f7f07605821fe396777fd6097412
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-25 22:25:31 +0100

    slots

---
+++ Section 6/slots/src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+};
+</script>
