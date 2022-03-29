1d4c5aab0d9fd0c005b9bff9e57cd702a3a695bc
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-29 10:46:48 +0100

    quiz app

---
+++ Section 8/quiz/src/components/Result.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIndex].title }}</div>
+    <div class="desc">{{ results[resultIndex].desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Result",
+  props: {
+    results: Array,
+    totalCorrect: Number,
+  },
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      this.results.forEach((e, i) => {
+        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+          index = i;
+        }
+      });
+
+      return index;
+    },
+  },
+};
+</script>
