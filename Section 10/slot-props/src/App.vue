faaae1709c0b78c414b88922c5015cf07aa18c8d
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-03-31 23:27:40 +0100

    slot properties

---
+++ Section 10/slot-props/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <hello-world v-slot="v">
+      <p>Hello {{v.user.name}}</p>
+    </hello-world> -->
+    <hello-world #default="{user, favorites}">
+      <p>Hello {{ user.name }}. I like {{favorites[0]}}.</p>
+
+      <p v-for="favorite in favorites" :key="favorite">{{favorite}}</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
