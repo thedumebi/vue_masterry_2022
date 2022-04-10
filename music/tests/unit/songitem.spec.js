76584ea3d0487a173fa80e7a808563060a0e81dc
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-04-10 07:12:23 +0100

    testing attributes

--- music/tests/unit/songitem.spec.js
+++ music/tests/unit/songitem.spec.js
@@ -21,6 +21,26 @@ describe("SongItem.vue", () => {
     // wrapper.find() works just like document.querySelector
     const compositionAuthor = wrapper.find(".text-gray-500");
 
-    // expect(compositionAuthor.text()).toBe(song.display_name);
+    expect(compositionAuthor.text()).toBe(song.display_name);
+  });
+
+  test("renders song.docID in id attribute", () => {
+    const song = {
+      docID: "abc",
+    };
+
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song,
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+
+    // expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`);
+    expect(wrapper.classes()).toContain(`song-id-${song.docID}`);
   });
 });
