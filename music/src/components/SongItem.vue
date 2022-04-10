db4b2d53a0c36f9d3617fa44490d2a14fc6318e2
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-04-10 11:11:38 +0100

    snapshot testing

--- music/src/components/SongItem.vue
+++ music/src/components/SongItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li
-    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50 example"
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50 example snapshot"
     :class="`song-id-${song.docID}`"
   >
     <div>
