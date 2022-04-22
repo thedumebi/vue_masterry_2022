f04006351e6322db243bc6fcc0759128f9cd1187
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2022-04-22 01:05:06 +0100

    vuex hooks

--- Section 21 - router_hooks/src/store/index.js
+++ Section 21 - router_hooks/src/store/index.js
@@ -2,11 +2,18 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    showAbout: false,
+  },
+  getters: {
+    showAbout(state) {
+      return state.showAbout;
+    },
   },
   mutations: {
+    setShowAbout(state, payload) {
+      state.showAbout = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
